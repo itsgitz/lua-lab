66bd88d130460dae19a0e33b4d5a7ed38b2cd352
itsgitz <anggit.ginanjar.dev@gmail.com>
2025-04-19 11:50:51 +0700

    chore: added getting started

---
+++ getting-started/hello.lua
@@ -0,0 +1,14 @@
+local name = "Anggit Ganteng"
+
+local hobbies = {
+	"Coding",
+	"Gaming",
+	"Reading",
+}
+
+local function greet(name)
+	return "Hello, " .. name .. "!"
+end
+
+print(greet(name))
+print("My hobbies are:", hobbies[1])
